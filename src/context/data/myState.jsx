4bc8bea73894419fa04992073075ce9354b50167
iHuzaifaEhsan <iamhuzaifaehsan@gmail.com>
2025-02-09 17:51:27 +0500

    Responsive Navbar with Dark and Light Mode

--- src/context/data/myState.jsx
+++ src/context/data/myState.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import MyContext from './myContext'
+import { useState } from 'react';
+import MyContext from './myContext';
 
-const myState = (props) => {
-  const state = {
-    name: "Kamal",
-    rollno: 15
-  }
+const MyState = (props) => {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'rgb(17, 24, 39)';
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  };
+
   return (
-    <MyContext.Provider value={state}>
+    <MyContext.Provider value={{ mode, toggleMode }}>
       {props.children}
     </MyContext.Provider>
-  )
-}
+  );
+};
 
-export default myState
+export default MyState;
