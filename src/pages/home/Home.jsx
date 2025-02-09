988fbdce6ac48f557917ce380a57e1551c72aa5b
iHuzaifaEhsan <iamhuzaifaehsan@gmail.com>
2025-02-10 01:08:53 +0500

    Home Page Designe

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,20 @@
-import React, { useContext } from 'react'
 import Layout from '../../components/layout/Layout.jsx'
 import HeroSection from '../../components/heroSection/HeroSection.jsx'
+import Filter from '../../components/filter/Filter.jsx'
+import ProductCard from '../../components/productCard/ProductCard.jsx'
+import Track from '../../components/track/Track.jsx'
+import Testimonial from '../../components/testimonial/Testimonial.jsx'
+import Footer from '../../components/footer/Footer.jsx'
 
 const Home = () => {
 
   return (
     <Layout>
       <HeroSection />
+      <Filter />
+      <ProductCard />
+      <Track />
+      <Testimonial />
     </Layout>
   )
 }
