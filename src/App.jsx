37b73981275b11c3037d2f2702722e882dde6bf9
iHuzaifaEhsan <iamhuzaifaehsan@gmail.com>
2025-02-10 09:59:30 +0500

    Remaining all Pages and Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,43 @@
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './pages/home/Home'
-import Order from './pages/order/Order'
-import Cart from './pages/cart/Cart'
-import Dashboard from './pages/admin/dashboard/Dashboard'
-import NoPage from './pages/nopage/NoPage'
-import MyState from './context/data/myState'
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+import Home from './pages/home/Home';
+import Order from './pages/order/Order';
+import Cart from './pages/cart/Cart';
+import Dashboard from './pages/admin/dashboard/Dashboard';
+import NoPage from './pages/nopage/NoPage';
+import MyState from './context/data/myState';
+import Login from './pages/registration/Login';
+import Signup from './pages/registration/Signup';
+import ProductInfo from './pages/productInfo/ProductInfo';
+import AddProduct from './pages/admin/page/AddProduct';
+import UpdateProduct from './pages/admin/page/UpdateProduct';
+
 
 function App() {
-
   return (
     <MyState>
       <Router>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/order' element={<Order />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/*' element={<NoPage />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/addproduct' element={<AddProduct />} />
+          <Route path='/updateproduct' element={<UpdateProduct />} />
+          <Route path="/productinfo/:id" element={<ProductInfo />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/*" element={<NoPage />} />
         </Routes>
       </Router>
     </MyState>
+
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,6 @@ import Filter from '../../components/filter/Filter.jsx'
 import ProductCard from '../../components/productCard/ProductCard.jsx'
 import Track from '../../components/track/Track.jsx'
 import Testimonial from '../../components/testimonial/Testimonial.jsx'
-import Footer from '../../components/footer/Footer.jsx'
 
 const Home = () => {
 
