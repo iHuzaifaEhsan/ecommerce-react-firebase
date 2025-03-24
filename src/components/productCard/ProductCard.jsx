0767fedfe35ea16ee3859f0a2deb59d0b120cbf5
iHuzaifaEhsan <iamhuzaifaehsan@gmail.com>
2025-03-24 12:59:42 +0500

    productCard.jsx correction

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import myContext from '../../context/data/myContext'
+import myContext from '../../context/data/MyContext'
 
 function ProductCard() {
     const context = useContext(myContext)
